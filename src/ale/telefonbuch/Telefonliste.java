80a51771248b58d18980440585b5a03e358fc8b5
andreasrieger <andreasrieger@me.com>
2020-11-04 19:55:45 +0100

    Added test class

--- src/ale/telefonbuch/Telefonliste.java
+++ src/ale/telefonbuch/Telefonliste.java
@@ -3,10 +3,56 @@
  */
 package ale.telefonbuch;
 
+import java.util.ArrayList;
+
 /**
  * @author Andreas Rieger
  *
  */
 public class Telefonliste {
-//ArrayList<TelefonEintrag>
+	
+	public TelefonEintrag[] eintrag;
+	private int entries = 0;
+	
+	private ArrayList<TelefonEintrag> telefonliste;
+//	private ArrayList<TelefonEintrag> telefonliste = new ArrayList<>();
+	
+	public Telefonliste() {
+		telefonliste = new ArrayList<>();
+	}
+
+	public Telefonliste(ArrayList<TelefonEintrag> telefonliste) {
+		
+	}
+	
+
+	
+	private void addEntry(String name, String number) {
+//		telefonliste.add(name);
+		eintrag[entries++] = name;
+	}
+	
+	private void modifyEntry(TelefonEintrag name, TelefonEintrag number, TelefonEintrag newname, TelefonEintrag newnumber) {
+		
+	}
+	
+	private void removeEntry(TelefonEintrag name, TelefonEintrag number) {
+		
+	}
+
+	private void searchByName(TelefonEintrag name, TelefonEintrag number) {
+		
+	}
+
+	private void searchNameByNumber(TelefonEintrag name, TelefonEintrag number) {
+		
+	}
+	
+	public int size() {
+		return telefonliste.size();
+	}
+	
+	public void clear() {
+		telefonliste.clear();
+	}
 }
